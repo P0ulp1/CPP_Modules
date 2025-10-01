29134ca29c06a58acff15a58bcdcd88d9ec3aece
Paul Hautenauve <phautena@f6r11s4.clusters.42paris.fr>
2025-10-01 18:23:09 +0200

    * CPP_07/ex00 --> OK * CPP_07/ex01 --> Not working anymore, need to research ******* function pointers

--- CPP_07/ex01/includes/iter.hpp
+++ CPP_07/ex01/includes/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: phautena <phautena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 13:28:31 by phautena          #+#    #+#             */
-/*   Updated: 2025/09/11 13:51:23 by phautena         ###   ########.fr       */
+/*   Updated: 2025/10/01 18:21:59 by phautena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 # include <string>
 # include <string.h>
 
-template <typename T, typename F>
-void	iter(T &array, size_t len, F func)
+template <typename T>
+void	iter(T &array, size_t len, void (*func)(T &))
 {
 	for (size_t i = 0; i < len; i++)
 		func(array[i]);
