bab7891c34e4e475ea01748d7292efe8cf0361e1
Paul Hautenauve <phautena@f1br4s5.clusters.42paris.fr>
2025-10-07 15:11:39 +0200

    * Added CPP_08/ex02

---
+++ CPP_08/ex02/includes/MutantStack.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phautena <phautena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 12:38:37 by phautena          #+#    #+#             */
+/*   Updated: 2025/10/07 13:18:07 by phautena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANT_STACK
+# define MUTANT_STACK
+
+# include <iostream>
+# include <deque>
+# include <vector>
+# include <list>
+# include <stack>
+
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+	public:
+		typedef typename Container::iterator		iterator;
+		typedef typename Container::const_iterator	const_iterator;
+
+		MutantStack(void) : std::stack<T, Container>() {};
+		~MutantStack(void) {};
+		MutantStack(const MutantStack &copy) : std::stack<T, Container>(copy) {};
+		MutantStack	&operator=(const MutantStack &rhs) {
+			if (this != &rhs)
+				std::stack<T, Container>::operator=(rhs);
+			return (*this);
+		}
+
+		iterator		begin() { return this->c.begin(); };
+		iterator		end()	{ return this->c.end(); };
+		const_iterator	begin() const { return this->c.begin(); };
+		const_iterator	end() const { return this->c.end(); };
+
+};
+
+#endif
---
+++ CPP_08/ex02/srcs/main.cpp
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phautena <phautena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/05 13:28:28 by phautena          #+#    #+#             */
+/*   Updated: 2025/10/07 13:15:35 by phautena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/MutantStack.hpp"
+
+int main(void)
+{
+	std::cout << "-------------------------SUBJECT TEST-------------------------" << std::endl;
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	std::cout << "-------------------------SUBJECT TEST-------------------------" << std::endl;
+
+	std::stack<int>							sstack;
+	MutantStack<int>						dstack;
+	MutantStack< int, std::list<int> >		lstack;
+	MutantStack< int, std::vector<int> >	vstack;
+
+	for (int i = 0; i < 5; i++) {
+		sstack.push(i + 1);
+		dstack.push(i + 1);
+		lstack.push(i + 1);
+		vstack.push(i + 1);
+	}
+
+	while (sstack.empty() == false) {
+		std::cout << "sstack: " << sstack.top() << std::endl;
+		sstack.pop();
+	}
+	std::cout << std::endl;
+
+	for (MutantStack<int>::iterator it = dstack.begin(); it != dstack.end(); it++)
+		std::cout << "dstack: " << *it << std::endl;
+	std::cout << std::endl;
+
+	for (MutantStack< int, std::vector<int> >::iterator it = vstack.begin(); it != vstack.end(); it++)
+	std::cout << "vstack: " << *it << std::endl;
+	std::cout << std::endl;
+
+	for (MutantStack< int, std::list<int> >::iterator it = lstack.begin(); it != lstack.end(); it++)
+		std::cout << "lstack: " << *it << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "-------------------------COPY TEST-------------------------" << std::endl;
+
+	MutantStack< int, std::list<int> >	copystack(lstack);
+	for (MutantStack<int, std::list<int> >::iterator it = copystack.begin(); it != copystack.end(); it++)
+		std::cout << "copystack: " << *it << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "-------------------------ASSIGNEMENT OPERATOR TEST-------------------------" << std::endl;
+
+	copystack.pop();
+	copystack.pop();
+	lstack = copystack;
+	for (MutantStack<int, std::list<int> >::iterator it = lstack.begin(); it != lstack.end(); it++)
+		std::cout << "lstack: " << *it << std::endl;
+
+	return (0);
+}
