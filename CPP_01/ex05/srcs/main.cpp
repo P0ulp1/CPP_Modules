a16c84be8c45a2b8dd4ccc06901423c9837d1d51
Paul Hautenauve <phautena@f1br9s8.clusters.42paris.fr>
2025-11-26 18:31:52 +0100

    * CPP_08/ex01 still not working

--- CPP_01/ex05/srcs/main.cpp
+++ CPP_01/ex05/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: phautena <phautena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 12:38:28 by phautena          #+#    #+#             */
-/*   Updated: 2025/05/12 12:44:02 by phautena         ###   ########.fr       */
+/*   Updated: 2025/11/14 17:32:10 by phautena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@ int	main(void)
 	std::cout << "Launching HARL.... Please Wait..." << std::endl;
 	std::cout << "Hello, I'm HARL. Starting demo mode for demonstration purposes." << std::endl;
 	std::cout << "Displaying my 4 level of complains." << "\n" << std::endl;
+
+
+
 	std::cout << "Level DEBUG:" << std::endl;
 	bot.complain("DEBUG");
 	std::cout << "\nLevel INFO:" << std::endl;
--- CPP_09/ex01/srcs/RPN.cpp
+++ CPP_09/ex01/srcs/RPN.cpp
@@ -21,6 +21,13 @@ RPN &RPN::operator=(const RPN &rhs)
 	return (*this);
 }
 
+static int is_digit(const int &c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+
 static int is_operator(const int &c)
 {
 	if (c == '+' || c == '-' || c == '/' ||  c == '*')
@@ -28,17 +35,56 @@ static int is_operator(const int &c)
 	return (0);
 }
 
+static int count_operators(const std::string &str)
+{
+	int count = 0;
+	for (size_t i = 0; i < str.size(); i++)
+	{
+		if (is_operator(str[i]))
+			count++;
+	}
+	return (count);
+}
+
+static int count_digits(const std::string &str)
+{
+	int count = 0;
+	for (size_t i = 0; i < str.size(); i++)
+	{
+		if (is_digit(str[i]))
+			count++;
+	}
+	return (count);
+}
+
 static int parsing(const std::string &str)
 {
-	int i = 0;
+	size_t i = 0;
+	int digits = count_digits(str);
+	int operators = count_operators(str);
+	if (digits != operators + 1) {
+		std::cout << "Digits: " << digits << " Operators: " << operators << std::endl;
+		return (1);
+	}
 	while (str[i])
 	{
-		if (i == 0 && !isdigit(str[i]))
+		if (i == 0 && !is_digit(str[i])) {
+			std::cout << "1" << std::endl;
 			return (1);
-		if (isdigit(str[i]) && isdigit(str[i + 1]))
+		}
+		if (i != 0 && is_digit(str[i]) && str[i - 1] != ' ' && str[i + 1] != ' ') {
+			std::cout << "2" << std::endl;
 			return (1);
-		if (str[i] == ' ' && (!is_operator(str[i + 1]) || isdigit(str[i + 1])))
+		}
+		if (i == str.size() && !is_operator(str[i])) {
+			std::cout << "3" << std::endl;
 			return (1);
+		}
+		if (i != 0 && i != str.size() && is_operator(str[i]) && str[i - 1] != ' ' && str[i + 1] != ' ') {
+			std::cout << "4" << std::endl;
+			return (1);
+		}
+		i++;
 	}
 	return (0);
 }
@@ -51,7 +97,43 @@ int RPN::calculate(const std::string &str)
 		return (1);
 	}
 
-	 
+	char c;
+	double res;
+	double a;
+	double b;
+	std::istringstream string(str);
+	while (string >> c)
+	{
+		if (is_operator(c))
+		{
+			if (_data.size() < 2)
+				return (1);
+
+			a = _data.top();
+			_data.pop();
+			b = _data.top();
+			_data.pop();
+
+			if (c == '-')
+				res = a - b;
+			else if (c == '+')
+				res = a + b;
+			else if (c == '*')
+				res = a * b;
+			else
+				res = a / b;
+
+			_data.pop();
+			_data.push(res);
+		}
+		else
+			_data.push(c - '0');
+	}
+
+	if (_data.size() != 1)
+		return (1);
+
+	std::cout << "Result is: " << _data.top() << std::endl;
 
 	return (0);
 }
